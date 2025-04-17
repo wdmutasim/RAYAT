ca8a92390a54cf660efddc7f10df1a817586dc00
wdmutasim <157717281+wdmutasim@users.noreply.github.com>
2025-04-17 13:13:34 +0300

    db.php

--- db.php
+++ db.php
@@ -1,12 +1,21 @@
 <?php
-$host = "localhost";
-$dbname = "rayat_db";
-$username = "root";
-$password = "";
+// اسم المضيف
+$host = "dpg-d00d45qli9vc739pj8ag-a";  // اسم المضيف من لوحة Render
+
+// اسم قاعدة البيانات
+$dbname = "rayat_db";  // اسم قاعدة البيانات في Render
+
+// اسم المستخدم
+$username = "rayat_db_user";  // اسم المستخدم في Render
+
+// كلمة المرور
+$password = "YOUR_PASSWORD";  // ضع كلمة المرور هنا
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    // الاتصال بقاعدة البيانات باستخدام PDO
+    $conn = new PDO("pgsql:host=$host;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "تم الاتصال بقاعدة البيانات بنجاح!";
 } catch (PDOException $e) {
     echo "فشل الاتصال: " . $e->getMessage();
 }
